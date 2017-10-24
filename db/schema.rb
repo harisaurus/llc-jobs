9fb63a19fec18a7e986b3de2a8789b3a0d71231f
Haris Mahmood <harismahmood89@gmail.com>
2017-10-24 17:22:02 -0400

    x

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024180556) do
+ActiveRecord::Schema.define(version: 20140629004552) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -108,22 +108,12 @@ ActiveRecord::Schema.define(version: 20171024180556) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["context"], name: "index_taggings_on_context"
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
   add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
   add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
-  add_index "taggings", ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
-  add_index "taggings", ["taggable_id"], name: "index_taggings_on_taggable_id"
-  add_index "taggings", ["taggable_type"], name: "index_taggings_on_taggable_type"
-  add_index "taggings", ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
-  add_index "taggings", ["tagger_id"], name: "index_taggings_on_tagger_id"
 
   create_table "tags", force: true do |t|
-    t.string  "name"
-    t.integer "taggings_count", default: 0
+    t.string "name"
   end
-
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "transactions", force: true do |t|
     t.integer  "job_post_id"
