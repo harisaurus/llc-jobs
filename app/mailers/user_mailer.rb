b8bbe27bd1410ddc6928e6b3c5547c95db68a5c6
Haris Mahmood <harismahmood89@gmail.com>
2017-11-02 11:08:05 -0400

    update all instances of llc to clc, update favicon to new clc logo

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "no-reply@ladieslearningcode.com"
+  default from: "no-reply@canadalearningcode.ca"
 
   def confirmation_email(email, job_post)
     @job_post = job_post
-    mail(to: email, subject: "Your Job Post with Ladies Learning Code")
+    mail(to: email, subject: "Your Job Post with Canada Learning Code")
   end
 
   def confirmation_email_for_feature(email, job_post)
